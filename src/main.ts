962fea62ee18b68b9f62c164cac209baea177e72
Dolphix <166293610+Dolphixcd@users.noreply.github.com>
2024-08-02 16:44:01 +0200

    navbar + routing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HomeContainerComponent } from './app/home/home-container/home-container.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      {
+        path: 'home',
+        component: HomeContainerComponent
+      }
+    ])
+  ],
+}).catch((err) => console.error(err));
