73762a63968f4c182e84c8a8ef482d12ff78045e
Dolphix <166293610+Dolphixcd@users.noreply.github.com>
2024-08-05 19:29:57 +0200

    auth unfinished

--- src/app/ladder/ladder-container/ladder-container.component.ts
+++ src/app/ladder/ladder-container/ladder-container.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PvPEntry } from './players.model';
 import { input } from '@angular/core';
+import { wow } from 'blizzard.js'
 
 @Component({
   selector: 'app-ladder-container',
@@ -14,12 +15,27 @@ export class LadderContainerComponent {
   private httpClient = inject(HttpClient);
   players: PvPEntry[] = [];
   isFetching = signal(false);
+  wowClient: any; // Declare the 'wowClient' property
 
   ladderpage = input.required<string>();
 
+  async initializeWowClient() {
+    this.wowClient = await wow.createInstance({
+      key: "020fcd3907a3449dbe86e8bdae328f17",
+      secret: "62tni4Z5xhRvVTk3u4RCtgubGvNbqqxq",
+      origin: 'eu', // optional
+      locale: 'en_US', // optional
+      token: '', // optional
+    });
+  }
+  
+
   ngOnInit() {
+    //Variable für das Loader Symbol
     this.isFetching.set(true);
-    this.httpClient.get<PvPEntry>('https://eu.api.blizzard.com/data/wow/pvp-season/37/pvp-leaderboard/3v3?namespace=dynamic-eu&locale=en_US&access_token=EUbORj7AFgFTb9x2KSeowasEuNb4Qt3r6E').subscribe((data: any) => {
+    this.initializeWowClient();
+    //API Call für die PvP Ladder
+    this.httpClient.get<PvPEntry>("https://eu.api.blizzard.com/data/wow/pvp-season/37/pvp-leaderboard/3v3?namespace=dynamic-eu&locale=en_US&access_token=" + this.wowClient).subscribe((data: any) => {
     this.players = data.entries.slice((parseInt(this.ladderpage()) * 100) - 100, parseInt(this.ladderpage()) * 100);
     this.isFetching.set(false);
   });
@@ -42,3 +58,6 @@ getNextLadderPage(): string {
 }
 
 }
+
+
+
